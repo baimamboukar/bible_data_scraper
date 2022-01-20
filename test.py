e7cb16ba8e057db6b332219b01354f99596a30ad
baimamboukar <baimamboukar@gmail.com>
2022-01-20 10:45:08 +0100

    testing get

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
-import json
-data = {
-    "Alkawal Kessal":{
-        "MATTA":{
-            "1": "Nda indee kaakirabr Yeesu Almasihu"
-        }
-    }
-}
-datax = {}
-datax['Alkawal Kessal'] = {}
-datax['Alkawal Kessal']['MARKUS'] = {}
-datax['Alkawal Kessal']['MARKUS']['1'] = "Linjila bana no Markus windi doum"
-print(datax['Alkawal Kessal'])
+# import json
+# data = {
+#     "Alkawal Kessal":{
+#         "MATTA":{
+#             "1": "Nda indee kaakirabr Yeesu Almasihu"
+#         }
+#     }
+# }
+# datax = {}
+# datax['Alkawal Kessal'] = {}
+# datax['Alkawal Kessal']['MARKUS'] = {}
+# datax['Alkawal Kessal']['MARKUS']['1'] = "Linjila bana no Markus windi doum"
+# print(datax['Alkawal Kessal'])
+
+import requests
+
+res = requests.get("https://my.bible.com/fr/bible/906/GEN.1.FB")
+print(res)
